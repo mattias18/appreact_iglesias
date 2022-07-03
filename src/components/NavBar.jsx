76cfdbd0a41c7b57cf9d90355c77a64252fe17c3
MatiasIglesias18 <48776624+MatiasIglesias18@users.noreply.github.com>
2022-07-03 20:26:23 -0300

    Segundo Commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { FcShop } from "react-icons/fc";
+
+export const NavBar = () => {
+    return (
+    <Navbar bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="#home">
+                <FcShop />
+                 GPUWorld
+                </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Contacto</Nav.Link>
+                <NavDropdown title="Productos" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">AMD</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Nvidia</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    )
+}
